890cd84260ccd3a0ab6f267ca1cfc280a7eaac0b
Matthew Barker <matthewbarker@librem.one>
2026-02-09 17:09:50 +0000

    refactor: align project with unified z\uksfta prefix and unique component VFS

--- addons/maps/config.cpp
+++ addons/maps/config.cpp
@@ -19,12 +19,12 @@ class CfgPatches {
 
 class CfgMods {
     author = "UKSF Taskforce Alpha Team";
-    logo = "z\uksfta\addons\main\data\icon_128_ca.paa";
-    logoOver = "z\uksfta\addons\main\data\icon_128_highlight_ca.paa";
-    logoSmall = "z\uksfta\addons\main\data\icon_64_ca.paa";
+    logo = "z\uksfta\maps\data\icon_128_ca.paa";
+    logoOver = "z\uksfta\maps\data\icon_128_highlight_ca.paa";
+    logoSmall = "z\uksfta\maps\data\icon_64_ca.paa";
     name = "UKSF Taskforce Alpha Maps";
     overview = "UKSF Taskforce Alpha";
-    picture = "z\uksfta\addons\main\data\title_co.paa";
+    picture = "z\uksfta\maps\data\title_co.paa";
     tooltip = "UKSFTA";
     tooltipOwned = "UKSF Taskforce Alpha";
 };
